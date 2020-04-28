b958bfcfe2954e10ca25cafbe2a61ab7b6a9ebb5
austinb847 <austinb847@icloud.com>
2020-04-28 09:19:25 -0700

    add styles to map and on mouseover and mouseout events

--- src/main.js
+++ src/main.js
@@ -18,15 +18,67 @@ L.Icon.Default.mergeOptions({
 
 $(document).ready(function() {
   let map = L.map('map').setView([37.8, -96], 4);
+  let geoJsonLayer;
   
 
   L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=' + process.env.ACCESS_TOKEN, {
+    maxZoom: 6,
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
+			'<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
+			'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     id: 'mapbox/light-v9',
     tileSize: 512,
     zoomOffset: -1
   }).addTo(map);
 
-  L.geoJson(statesData).addTo(map);
+  function style() {
+    return {
+      fillColor: 'red',
+      weight: 2,
+      opacity: 1, 
+      color: 'white',
+      dashArray: '3',
+      fillOpacity: 0.7
+    };
+  }
+
+  function highlightFeature(e) {
+    let layer = e.target;
+
+    layer.setStyle({
+      weight: 5,
+      color: '#666',
+      dashArray: '',
+      fillOpacity: 0.7
+    });
+
+    if (!L.Browser.ie && !L.Browser.opera && !L.Browser.edge) {
+      layer.bringToFront();
+    }
+  }
+
+  function resetHighlight(e) {
+    geoJsonLayer.resetStyle(e.target);
+  }
+
+  
+
+  function onEachFeature(feature, layer) {
+    layer.on({
+      mouseover: highlightFeature,
+      mouseout: resetHighlight,
+      /* click: zoomToFeature */
+    });
+  }
+
+  
+
+  geoJsonLayer = L.geoJson(statesData, {
+    style: style,
+    onEachFeature: onEachFeature
+  }).addTo(map);
+
+  
 
   
 });
