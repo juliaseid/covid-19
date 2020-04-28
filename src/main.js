5e0a0806fefaec2ec249ad1abf049f4d0da7fe7b
austinb847 <austinb847@icloud.com>
2020-04-27 17:26:51 -0700

    add initial Leaflet map, get GEOJson data

--- src/main.js
+++ src/main.js
@@ -2,3 +2,31 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
+import L from 'leaflet';
+import 'leaflet/dist/leaflet.css';
+import { statesData } from './JSON/us-states.js';
+
+
+delete L.Icon.Default.prototype._getIconUrl;
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl: require('leaflet/dist/images/marker-icon-2x.png'),
+  iconUrl: require('leaflet/dist/images/marker-icon.png'),
+  shadowUrl: require('leaflet/dist/images/marker-shadow.png'),
+});
+
+
+
+$(document).ready(function() {
+  let map = L.map('map').setView([37.8, -96], 4);
+  
+
+  L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=' + process.env.ACCESS_TOKEN, {
+    id: 'mapbox/light-v9',
+    tileSize: 512,
+    zoomOffset: -1
+  }).addTo(map);
+
+  L.geoJson(statesData).addTo(map);
+
+  
+});
